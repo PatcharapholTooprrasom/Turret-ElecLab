6483e6b01c835af2f2127f2ac65056f8f2e9e43d
PatcharapholToopprasom <bignorth46@gmail.com>
2025-03-04 18:32:58 +0700

    Add relay control functionality and update display information

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <Fonts/Picopixel.h>
 
 const int IR_PIN = PA0;
+const int RELAY1_PIN = PA3;
+const int RELAY2_PIN = PA4;
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -25,107 +27,113 @@ const int SERVO180_STEP = 1;
 const unsigned long CMD_TIMEOUT = 200;
 const unsigned long REPEAT_DELAY = 300;
 
+bool relay1State = false;
+bool relay2State = false;
+
 IRrecv irrecv(IR_PIN);
 
 unsigned long lastCmdTime = 0;
-enum Command
-{
+enum Command {
   NONE,
   UP,
   DOWN,
   LEFT,
   RIGHT,
-  OK
+  OK,
+  STAR,
+  SQUARE
 };
 Command lastCommand = NONE;
 bool newPress = false;
 
-int displayangle(int angle)
-{
+int displayangle(int angle) {
   if (angle < 92)
     return 90 - (92 - angle) * 44 / 45;
   else
     return 90 + (angle - 92) * 43 / 45;
 }
 
-void updateDisplay()
-{
+void updateDisplay() {
   display.clearDisplay();
-  display.setCursor(0, 10);
+  display.setCursor(0, 0);
   display.println("Servo Control");
-  display.print("180 deg: ");
+  display.print("180: ");
   display.println(displayangle(servo180Pos));
-  display.print("360 state: ");
+  display.print("360: ");
   display.println(servo360Target);
+  display.print("Relay1: ");
+  display.println(relay1State ? "ON" : "OFF");
+  display.print("Relay2: ");
+  display.println(relay2State ? "ON" : "OFF");
   display.display();
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
   irrecv.enableIRIn();
+
+  pinMode(RELAY1_PIN, OUTPUT);
+  pinMode(RELAY2_PIN, OUTPUT);
+  digitalWrite(RELAY1_PIN, LOW);
+  digitalWrite(RELAY2_PIN, LOW);
 
   servo360.attach(SERVO360_PIN);
   servo180.attach(SERVO180_PIN);
   servo180.write(servo180Pos);
   servo360.write(servo360Target);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
-  {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println("OLED failed");
-    while (1)
-      ;
+    while (1);
   }
   display.setFont(&Picopixel);
-  display.setTextSize(2);
+  display.setTextSize(2);  // Reduced size to fit all information
   display.setTextColor(SSD1306_WHITE);
   updateDisplay();
 }
 
-void processIRCommand()
-{
+void processIRCommand() {
   unsigned long rawData = irrecv.decodedIRData.decodedRawData;
   bool isRepeat = irrecv.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT;
 
-  if (!isRepeat)
-  {
+  if (!isRepeat) {
     newPress = true;
     lastCommand = NONE;
-
     if (rawData == 3877175040)
       lastCommand = UP;
     else if (rawData == 2907897600)
       lastCommand = DOWN;
-    else if (rawData == 4144561920)
-    {
+    else if (rawData == 4144561920) {
       servo360Target = 180;
       lastCommand = LEFT;
-    }
-    else if (rawData == 2774204160)
-    {
+    } else if (rawData == 2774204160) {
       servo360Target = 0;
       lastCommand = RIGHT;
-    }
-    else if (rawData == 3810328320)
-    {
+    } else if (rawData == 3810328320) {
       lastCommand = OK;
+    } else if (rawData == 3910598400) {  // STAR pressed
+      relay1State = !relay1State;
+      digitalWrite(RELAY1_PIN, relay1State ? HIGH : LOW);
+      lastCommand = STAR;
+    } else if (rawData == 4061003520) {  // SQUARE pressed
+      relay2State = !relay2State;
+      digitalWrite(RELAY2_PIN, relay2State ? HIGH : LOW);
+      lastCommand = SQUARE;
     }
   }
 
   // Handle movement
-  if ((newPress || isRepeat) && lastCommand != NONE)
-  {
-    switch (lastCommand)
-    {
-    case UP:
-      servo180Pos = constrain(servo180Pos + SERVO180_STEP, 90 - servo180range, 90 + servo180range + 5);
-      break;
-    case DOWN:
-      servo180Pos = constrain(servo180Pos - SERVO180_STEP, 90 - servo180range, 90 + servo180range - 5);
-      break;
-    case OK:
-      servo180Pos = 92;
-      break;
+  if ((newPress || isRepeat) && lastCommand != NONE) {
+    switch (lastCommand) {
+      case UP:
+        servo180Pos = constrain(servo180Pos + SERVO180_STEP, 90 - servo180range, 90 + servo180range + 5);
+        break;
+      case DOWN:
+        servo180Pos = constrain(servo180Pos - SERVO180_STEP, 90 - servo180range, 90 + servo180range - 5);
+        break;
+      case OK:
+        servo180Pos = 92;
+        break;
     }
     newPress = false;
   }
@@ -134,8 +142,7 @@ void processIRCommand()
   servo360.write(servo360Target);
 
   static int last180DisplayPos = -1;
-  if (servo180Pos != last180DisplayPos)
-  {
+  if (servo180Pos != last180DisplayPos || relay1State != digitalRead(RELAY1_PIN) || relay2State != digitalRead(RELAY2_PIN)) {
     last180DisplayPos = servo180Pos;
     updateDisplay();
   }
@@ -143,26 +150,21 @@ void processIRCommand()
   lastCmdTime = millis();
 }
 
-void loop()
-{
-  if (millis() - lastCmdTime > CMD_TIMEOUT)
-  {
+void loop() {
+  if (millis() - lastCmdTime > CMD_TIMEOUT) {
     servo360Target = 90;
     servo360.write(servo360Target);
   }
 
-  if (irrecv.decode())
-  {
+  if (irrecv.decode()) {
     processIRCommand();
     irrecv.resume();
   }
 
   // Debounce logic for single press
   static unsigned long lastRepeatTime = 0;
-  if (lastCommand == UP || lastCommand == DOWN)
-  {
-    if (millis() - lastRepeatTime > REPEAT_DELAY && !irrecv.isIdle())
-    {
+  if (lastCommand == UP || lastCommand == DOWN) {
+    if (millis() - lastRepeatTime > REPEAT_DELAY && !irrecv.isIdle()) {
       processIRCommand();
       lastRepeatTime = millis();
     }
